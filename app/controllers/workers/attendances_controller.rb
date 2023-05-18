ae7306799893a9b7576e941f3b08d955d9bb04eb
EC2 Default User <chyou726@outlook.jp>
2023-05-18 13:42:11 +0000

    打刻ができるように再編集

--- app/controllers/workers/attendances_controller.rb
+++ app/controllers/workers/attendances_controller.rb
@@ -43,30 +43,33 @@ class Workers::AttendancesController < ApplicationController
     number_of_month.times do |i|
       i += 1
       day = i
+
+
       if t["id"][i.to_s].present?
         attendance = Attendance.find(t["id"][i.to_s])
-        start_time = t["start_worktime"][i.to_s]
-        finish_time = t["finish_worktime"][i.to_s]
-        start_breaktime = t["start_breaktime"][i.to_s]
-        finish_breaktime = t["finish_breaktime"][i.to_s]
+        start_time = t["start_worktime"][i.to_s] || nil
+        finish_time = t["finish_worktime"][i.to_s] || nil
+        start_breaktime = t["start_breaktime"][i.to_s] || nil
+        finish_breaktime = t["finish_breaktime"][i.to_s] || nil
 
-        start_worktime = "#{year}-#{month}-#{day} #{start_time}"
-        finish_worktime = "#{year}-#{month}-#{day} #{finish_time}"
-        start_breaktime = "#{year}-#{month}-#{day} #{start_breaktime}"
-        finish_breaktime = "#{year}-#{month}-#{day} #{finish_breaktime}"
+        start_worktime = start_time.present? ? "#{year}-#{month}-#{day} #{start_time}" : nil
+        finish_worktime = finish_time.present? ? "#{year}-#{month}-#{day} #{finish_time}" : nil
+        start_breaktime = start_breaktime.present? ? "#{year}-#{month}-#{day} #{start_breaktime}" : nil
+        finish_breaktime = finish_breaktime.present? ? "#{year}-#{month}-#{day} #{finish_breaktime}" : nil
 
         attendance.update(start_worktime: start_worktime, finish_worktime: finish_worktime, start_breaktime: start_breaktime, finish_breaktime: finish_breaktime, comment: t["comment"][i.to_s], reason_status: Attendance.reason_statuses.keys[t["reason_status"][i.to_s].to_i])
       else
         Attendance.create(
           worker_id: params[:id],
-          start_worktime: DateTime.new(start_time),
-          finish_worktime: t["finish_worktime"][i.to_s],
-          start_breaktime: t["start_breaktime"][i.to_s],
-          finish_breaktime: t["finish_breaktime"][i.to_s],
-          comment: t["comment"][i.to_s],
+          start_worktime: start_time.present? ? DateTime.new(start_time) : nil,
+          finish_worktime: t["finish_worktime"][i.to_s] || nil,
+          start_breaktime: t["start_breaktime"][i.to_s] || nil,
+          finish_breaktime: t["finish_breaktime"][i.to_s] || nil,
+          comment: t["comment"][i.to_s] || nil,
           stamp_date: params[:month].present? ? Date.new(Date.current.year, params[:month].to_i, i) : Date.new(Date.current.year, Date.current.month, i),
           reason_status: Attendance.reason_statuses.keys[t["reason_status"][i.to_s].to_i]
         )
+
       end
     end
       flash[:notice] = "You have updated attendance successfully."
