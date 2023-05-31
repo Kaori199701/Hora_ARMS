75c6dbf0132c514b4e89aa9bc9c4fa1edbb38391
Kaori199701 <chyou726@outlook.jp>
2023-05-31 12:30:57 +0000

    打刻漏れの通知コントローラーを作成

--- app/controllers/workers/homes_controller.rb
+++ app/controllers/workers/homes_controller.rb
@@ -2,17 +2,53 @@ class Workers::HomesController < ApplicationController
   def top
     @attendance = Attendance.new
     @worker = current_worker
-    @informations = Attendance.all
+    @attendances = Attendance.where(worker_id: @worker.id)
 
+    attendances = Attendance.where(worker_id: @worker.id).where("stamp_date >= ?", DateTime.now.at_beginning_of_month).where("stamp_date <= ?", DateTime.now.at_end_of_month)
+    day_num = DateTime.now.at_end_of_month.day
+    start_worktimes = attendances.pluck(:start_worktime)
+    finish_worktimes = attendances.pluck(:finish_worktime)
+    start_breaktimes = attendances.pluck(:start_breaktime)
+    finish_breaktimes = attendances.pluck(:finish_breaktime)
 
-    if params[:start_worktime].nil? # :start_worktime, :finish_worktime, :start_breaktime, :finish_breaktime に値がないとき
-      @infomations = Attendance.where(id: params[:start_worktime]) #打刻がない日付を探す
-      #●月●日に打刻漏れがあります。(1か月単位？)
-    else  #上4つに値がある場合
-      #何も表示しない
+    errors = []
+
+    if start_worktimes.select(&:itself).size < day_num || finish_worktimes.select(&:itself).size < day_num || start_breaktimes.select(&:itself).size < day_num || finish_breaktimes.select(&:itself).size < day_num
+            #打刻した日数が、月の日数より少ない場合
+      @attendances.each do |attendance|
+
+        error_attendance = false
+
+        if attendance.start_worktime.nil?
+          error_attendance = true
+        end
+
+        if attendance.finish_worktime.nil?
+          error_attendance = true
+        end
+
+        if attendance.start_breaktime.nil?
+          error_attendance = true
+        end
+
+        if attendance.finish_breaktime.nil?
+          error_attendance = true
+        end
+
+        if error_attendance
+          errors.push("●月●日に打刻漏れがあります")
+        end
+      end
+
+      # if @informations.  .nil? # :start_worktime, :finish_worktime, :start_breaktime, :finish_breaktime に値がない日
+      #   @infomations = Attendance.where(id: params[:start_worktime]) #打刻がない日付を探す
+      #       #●月●日に打刻漏れがあります。(1か月単位？)
+      # else  #上4つに値がある場合
+      #       #何も表示しない
+      # end
+    else
+
     end
-
-
   end
 
   def create
