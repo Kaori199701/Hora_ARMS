83873a393cc39a7383055ff14c9b42d12866e81b
Kaori199701 <chyou726@outlook.jp>
2023-06-25 19:45:57 +0900

    Workerトップ画面の時間の自動更新を実装

--- app/helpers/admins/excels_helper.rb
+++ app/helpers/admins/excels_helper.rb
@@ -12,16 +12,33 @@ module Admins::ExcelsHelper
     end
 
     if attendance_start_worktime
-      if hour2 >= hour1 && minute2 > minute1    #17:55 17:56
+      if hour2 >= hour1 && minute2 > minute1    #8:55 8:56
         "#{hour2 - hour1}:#{minute2 - minute1}"
-      elsif hour2 > hour1 && minute2 <= minute1 #17:59 18:00
+      elsif hour2 > hour1 && minute2 <= minute1 #8:59 9:00
         "#{hour2 - hour1 - 1}:#{minute2 + minute1 }"
       end
     end
   end
 
   #早退
+  def leave_early(finish_working_hour, attendance_finish_worktime)
+    hour1 = finish_working_hour.to_s.match(/\d{2}:\d{2}/)[0].split(":")[0].to_i
+    if attendance_finish_worktime
+      hour2 = attendance_finish_worktime.to_s.match(/\d{2}:\d{2}/)[0].split(":")[0].to_i
+    end
+    minute1 = finish_working_hour.to_s.match(/\d{2}:\d{2}/)[0].split(":")[1].to_i
+    if attendance_finish_worktime
+      minute2 = attendance_finish_worktime.to_s.match(/\d{2}:\d{2}/)[0].split(":")[1].to_i
+    end
 
+    if attendance_finish_worktime
+      if hour1 >= hour2 && minute1 > minute2    #17:30 17:05
+        "#{hour1 - hour2}:#{minute1 - minute2}"
+      elsif hour2 > hour1 && minute2 <= minute1 #17:30 16:50
+        "#{hour2 - hour1 - 1}:#{minute2 + minute1 }"
+      end
+    end
+  end
 
   #残業
 
